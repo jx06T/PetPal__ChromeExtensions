fc85b423ec1b707572090ab5f9cde3d930ec63ca
jx06T <50313tjx06@gmail.com>
2023-06-04 20:43:16 +0800

    modified:   content.js

--- content.js
+++ content.js
@@ -88,15 +88,13 @@ class aPet {
         if (fishes.length > 0) {
             if (this.state != 6) {
                 this.state = 6
-                // MyPet.timer = 12
                 this.timer = Math.random() * 4 + 10
                 this.img.src = IMG_URL + "pet_walk.gif"
             } else if (this.distance < 0.5) {
                 this.food.eaten()
                 this.img.src = IMG_URL + "pet_rest.gif"
                 this.state = 5
-                // this.timer = 26
-                MyPet.timer = Math.random() * 4 + 24
+                this.timer = Math.random() * 4 + 24
                 this.vx = 0
                 this.vy = 0
                 ResetAllPet()
@@ -107,7 +105,7 @@ class aPet {
             if (this.state == 0) {
                 this.state = 1
                 // this.timer = 9
-                MyPet.timer = Math.random() * 4 + 7
+                this.timer = Math.random() * 4 + 7
                 this.img.src = IMG_URL + "pet_walk.gif"
             } else if (this.state == 4 || this.state == 1 || this.state == 2) {
                 this.state = this.state == 4 ? 5 : 0
@@ -122,7 +120,7 @@ class aPet {
                 this.img.src = IMG_URL + "pet_walk.gif"
                 this.state = 2
                 // this.timer = 22
-                MyPet.timer = Math.random() * 4 + 20
+                this.timer = Math.random() * 4 + 20
                 this.distance = 0
                 return
             }
@@ -132,7 +130,7 @@ class aPet {
                 this.state = 4
                 this.img.src = IMG_URL + "pet_walk.gif"
                 // this.timer = 5
-                MyPet.timer = Math.random() * 4 + 3
+                this.timer = Math.random() * 4 + 3
             } else if (this.touchM > 8) {
                 this.distance = 0
             }
@@ -190,7 +188,7 @@ class aFish {
 // const MyPet = new aPet(...GetRandXY(), 120)
 let fishes = []
 let Pets = []
-for (let i = 0; i < 300; i++) {
+for (let i = 0; i < 30; i++) {
     Pets.push(new aPet(...GetRandXY(), 120))
 }
 setInterval(() => {
