6b74e64c8256bc06dfba7250b1a628eced328651
jx06T <50313tjx06@gmail.com>
2023-07-08 23:32:46 +0800

    modified:   content.js 	modified:   images/Sharp.png

--- content.js
+++ content.js
@@ -255,7 +255,7 @@ async function initialPet() {
 // -----------------------------------------------------------------------
 let styleElement = document.createElement('style');
 let isSharp = false
-styleElement.textContent = '*{cursor: url("https://raw.githubusercontent.com/jx06T/PetPal__ChromeExtensions/main/images/Penguin.gif"), grab!important;'
+styleElement.textContent = '*{cursor: url("https://raw.githubusercontent.com/jx06T/PetPal__ChromeExtensions/main/images/Sharp.png"), grab!important;'
 function Sharp() {
     if (!isSharp) {
         // document.body.classList.add("jx06Cbody")
